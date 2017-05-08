abbb4d8082cc753219c905eac30bef959f7dfe61
mleduc714 <michael_leduc@comcast.net>
2017-05-08 11:15:54 -0400

    update

---
+++ MRL_714/src/main/java/MRL714/MRL_714/App.java
@@ -0,0 +1,13 @@
+package MRL714.MRL_714;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
